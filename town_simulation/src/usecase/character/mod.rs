6a0280801f4d423ea8d3e3b5c3d481fa8772c6b7
Orchaldir <orchaldir@gmail.com>
2021-12-21 12:04:03 +0100

    [#3] Refactor 2

--- town_simulation/src/usecase/character/mod.rs
+++ town_simulation/src/usecase/character/mod.rs
@@ -14,37 +14,37 @@ pub fn create_child(
     mother: CharacterId,
 ) -> CharacterId {
     let parents = [father, mother].into();
-    let child_id = manager.create();
+    let child = manager.create();
     let siblings = get_shared_children(manager, father, mother);
     let grandparents = combine(&parents, |id| get_parents(manager, id));
     let piblings = combine(&parents, |id| get_siblings(manager, id));
     let cousins = combine(&piblings, |id| get_children(manager, id));
 
-    add_relation(manager, child_id, &grandparents, GrandChild);
-    add_relation(manager, child_id, &cousins, Cousin);
-    add_relation(manager, child_id, &piblings, Nibling);
-    add_relation(manager, child_id, &siblings, Sibling);
-    add_relation(manager, child_id, &parents, Child);
+    add_relation(manager, child, &grandparents, GrandChild);
+    add_relation(manager, child, &cousins, Cousin);
+    add_relation(manager, child, &piblings, Nibling);
+    add_relation(manager, child, &siblings, Sibling);
+    add_relation(manager, child, &parents, Child);
 
-    child_id
+    child
 }
 
 fn add_relation(
     manager: &mut CharacterMgr,
-    character_id: CharacterId,
-    other_ids: &HashSet<CharacterId>,
+    character: CharacterId,
+    others: &HashSet<CharacterId>,
     relation_type: RelationType,
 ) {
-    let relation = Relation::new(relation_type, character_id);
+    let relation = Relation::new(relation_type, character);
     let other_type = relation_type.reverse();
 
-    for other_id in other_ids {
-        manager.get_mut(*other_id).unwrap().relations.push(relation);
+    for other in others {
+        manager.get_mut(*other).unwrap().relations.push(relation);
 
-        let other_relation = Relation::new(other_type, *other_id);
+        let other_relation = Relation::new(other_type, *other);
 
         manager
-            .get_mut(character_id)
+            .get_mut(character)
             .unwrap()
             .relations
             .push(other_relation);
