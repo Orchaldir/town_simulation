266a217f3e25645b8afa55682e4a7b0d45f84b32
Orchaldir <orchaldir@gmail.com>
2021-12-21 15:53:29 +0100

    Use CharacterMgr as state

--- town_viewer/src/main.rs
+++ town_viewer/src/main.rs
@@ -1,12 +1,45 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::State;
+use town_simulation::model::character::CharacterMgr;
+use town_simulation::usecase::character::create_child;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn get_characters(characters: &State<CharacterMgr>) -> String {
+    format!("The town has {} characters!", characters.get_all().len())
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    let characters = init_characters();
+
+    rocket::build()
+        .manage(characters)
+        .mount("/", routes![get_characters])
+}
+
+fn init_characters() -> CharacterMgr {
+    let mut manager = CharacterMgr::default();
+
+    // generation 0
+    let grandfather0 = manager.create();
+    let grandmother0 = manager.create();
+    let grandfather1 = manager.create();
+    let grandmother1 = manager.create();
+
+    // generation 1
+    let father = create_child(&mut manager, grandfather0, grandmother0);
+    let aunt = create_child(&mut manager, grandfather0, grandmother0);
+    let mother = create_child(&mut manager, grandfather1, grandmother1);
+    create_child(&mut manager, grandfather1, grandmother1);
+    let husband_aunt = manager.create();
+
+    // generation 2
+    create_child(&mut manager, father, mother);
+    create_child(&mut manager, father, mother);
+    create_child(&mut manager, father, mother);
+    create_child(&mut manager, husband_aunt, aunt);
+
+    manager
 }
